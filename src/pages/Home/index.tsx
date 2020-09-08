ea267c16228eb4f835de78307544af1b018dd49b
Mikaio Melo <mikaiomelo23@gmail.com>
2020-09-07 22:33:39 -0300

    Home page (not completed)

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './styles.css';
+
+function Home() {
+    return (
+        <div id="home-page">
+            <h1>HI</h1>
+        </div>
+    );
+}
+
+export default Home;
