1136f4f725d3424bc9d6c0af295544e8a54c87d0
Mikaio Melo <mikaiomelo23@gmail.com>
2020-09-07 22:32:19 -0300

    Update routes file

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
+import Login from './pages/Login';
 
 function Routes() {
     return (
         <BrowserRouter>
             <Route path='/' component={Home} exact />
+            <Route path='/login' component={Login} />
         </BrowserRouter>
     );
 }
